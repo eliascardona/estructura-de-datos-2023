eca1095ff57d0b01823bf36fa6cbf51863f514fc
Elias Cardona <eliascardonarod@gmail.com>
2023-09-06 07:36:44 -0600

    06-SEP-2023. Commit #3. Reorganizando carpetas y eliminando archivos ejecutables

--- aula/sem5/struct-atletas.cpp
+++ aula/sem5/struct-atletas.cpp
@@ -1,17 +1,74 @@
+// Elias Eduardo Cardona Rodríguez
+// LITC 3A
+// 07-SEP-2023
+// Objetivo: Determinar cual a que categoria pertenece un corredor segun su edad
+
 #include <iostream>
-#include <iomanip>
+#include <cstring>
 using namespace std;
+
+#define K 5
 
 struct Corredor {
     char nombre[30];
-    char clubnom[25];
+    char genero[2];
     char categoria[12];
-    char genero;
+    char club[25];
     int edad;
 };
 
+void registrarCorredores(Corredor corredores[]);
+void determinarCategoria(Corredor& corredor);
+
+
+int main() {
+    Corredor corredores[K];
+    cout<<"Ingrese los datos de los corredores:"<<endl;
+    registrarCorredores(corredores);
+
+
+    cout<<"Reporte de corredores:"<<endl;
+    for(int i=0; i<K; i++) {
+        cout<<"****DATOS DEL CORREDOR****"<<endl;
+        cout<<"Nombre corredor: "<<corredores[i].nombre<<endl;
+        cout<<"Edad del corredor: "<<corredores[i].edad<<endl;
+        cout<<"Genero corredor: "<<corredores[i].genero<<endl;
+        cout<<"Categoria del corredor: "<<corredores[i].categoria<<endl;
+        cout<<"Nombre del club: "<<corredores[i].club<<endl<<endl;
+    }
+
+
+	cout<<endl<<endl;
+    return 0;
+}
+
+
+void registrarCorredores(Corredor corredores[]) {
+    for(int i=0; i<K; i++) {
+        cout<<"Corredor #"<<i+1<<endl;
+        cout<<"Nombre: ";
+        cin.getline(corredores[i].nombre, 30);
+        cin.ignore();
+
+        cout<<"Edad: ";
+        cin>>corredores[i].edad;
+        cin.ignore();
+
+        cout<<"Genero (M/F): ";
+        cin>>corredores[i].genero;
+        cin.ignore();
+
+        cout<<"Nombre del club: ";
+        cin.getline(corredores[i].club, 25);
+        cin.ignore();
+
+        determinarCategoria(corredores[i]);
+    }
+}
+
+
 void determinarCategoria(Corredor& corredor) {
-    if (corredor.edad > 40) {
+    if(corredor.edad > 40) {
         strcpy(corredor.categoria, "Veterano");
     } else if (corredor.edad >= 19 && corredor.edad <= 40) {
         strcpy(corredor.categoria, "Adulto");
@@ -20,39 +77,6 @@ void determinarCategoria(Corredor& corredor) {
     }
 }
 
-int main() {
-    const int numCorredores = 5;
-    Corredor corredores[numCorredores];
-
-    cout << "Ingrese los datos de los corredores:" << endl;
-
-    for (int i = 0; i < numCorredores; i++) {
-        cout << "Corredor #" << i + 1 << endl;
-        cout << "Nombre: ";
-        cin.ignore();
-        cin.getline(corredores[i].nombre, 30);
-        cout << "Edad: ";
-        cin >> corredores[i].edad;
-        cout << "Género (M/F): ";
-        cin >> corredores[i].genero;
-        cout << "Nombre del club: ";
-        cin.ignore();
-        cin.getline(corredores[i].clubnom, 25);
-        determinarCategoria(corredores[i]);
-    }
-
-    cout << "Reporte de corredores:" << endl;
-
-    for (int i = 0; i < numCorredores; i++) {
-        cout << "****DATOS DEL CORREDOR****" << endl;
-        cout << "Nombre corredor: " << corredores[i].nombre << endl;
-        cout << "Edad del corredor: " << corredores[i].edad << endl;
-        cout << "Genero corredor: " << corredores[i].genero << endl;
-        cout << "Categoría del corredor: " << corredores[i].categoria << endl;
-        cout << "Nombre del club: " << corredores[i].clubnom << endl;
-        cout << endl;
-    }
 
 
-    return 0;
-}
+
